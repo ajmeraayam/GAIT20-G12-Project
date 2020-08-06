1af86337ca6ffd674898bc3a4fa3091dd22e58b1
rmit-s3722631-Daniel-Snowsill <41409058+rmit-s3722631-Daniel-Snowsill@users.noreply.github.com>
2020-08-06 14:20:42 +1000

    Basic 'Persue" behaviour
    added Persue behavior that persues player position only

---
+++ GAIT-G12/Assets/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rigidBody;
+    private GameObject player;
+    private Vector2 movement;
+    private float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //use vector subtraction to find vector between player and current object
+        Vector2 direction = player.transform.position - gameObject.transform.position;
+        direction.Normalize();
+        rigidBody.MovePosition((Vector2)gameObject.transform.position + (direction * speed * Time.deltaTime));
+    }
+}
