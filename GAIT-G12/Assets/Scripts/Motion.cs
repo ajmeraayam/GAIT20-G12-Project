35efdd2ec695528da1c77af4d0692210cd9c120c
ajmeraayam <ajmeraayam@gmail.com>
2020-08-10 17:24:29 +1000

    Seek behaviour implemented for basic objects

---
+++ GAIT-G12/Assets/Scripts/Motion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour
+{
+    private bool collided = false;
+    [Tooltip("Positive value to move forward\nNegative to move backward")][SerializeField] float horizontalspeed = 2f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(!collided)
+        {
+            transform.Translate(horizontalspeed * Vector3.forward * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        collided = true;
+    }
+}
