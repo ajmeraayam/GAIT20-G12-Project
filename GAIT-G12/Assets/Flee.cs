8d0591a55f6c1a79b6e2247b8a8fe98cb5d5af01
rmit-s3722631-Daniel-Snowsill <41409058+rmit-s3722631-Daniel-Snowsill@users.noreply.github.com>
2020-08-10 20:43:24 +1000

    Added basic flee

--- GAIT-G12/Assets/Flee.cs
+++ GAIT-G12/Assets/Flee.cs
@@ -1,28 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
-{
-
+public class Flee : MonoBehaviour {
     private Rigidbody2D rigidBody;
-    private GameObject player;
+    private GameObject enemy;
     private Vector2 movement;
     private float speed = 5.0f;
+    private float angle;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         rigidBody = this.GetComponent<Rigidbody2D>();
-        player = GameObject.FindWithTag("Player");
+        enemy = GameObject.FindWithTag("Enemy");
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        //use vector subtraction to find vector between player and current object
-        Vector2 direction = player.transform.position - gameObject.transform.position;
+    void Update() {
+
+        //use vector subtraction to find vector between enemy and current object
+        Vector2 direction = -(enemy.transform.position - gameObject.transform.position);
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
+        rigidBody.rotation = angle;
         direction.Normalize();
         rigidBody.MovePosition((Vector2)gameObject.transform.position + (direction * speed * Time.deltaTime));
     }
---
+++ GAIT-G12/Assets/Persue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persue : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private GameObject player;
+    private Vector2 movement;
+    private float speed = 5.0f;
+    private float angle;
+    // Start is called before the first frame update
+    void Start() {
+        rigidBody = this.GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //use vector subtraction to find vector between player and current object
+        Vector2 direction = player.transform.position - gameObject.transform.position;
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
+        rigidBody.rotation = angle;
+        direction.Normalize();
+        rigidBody.MovePosition((Vector2)gameObject.transform.position + (direction * speed * Time.deltaTime));
+    }
+}
