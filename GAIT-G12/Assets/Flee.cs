f27c1460e7f993186ce33ec622b08c68dd62973a
rmit-s3722631-Daniel-Snowsill <41409058+rmit-s3722631-Daniel-Snowsill@users.noreply.github.com>
2020-08-10 21:13:39 +1000

    Added steering to Persue and Flee

--- GAIT-G12/Assets/Flee.cs
+++ GAIT-G12/Assets/Flee.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Flee : MonoBehaviour {
     private Rigidbody2D rigidBody;
     private GameObject enemy;
-    private Vector2 movement;
+    private Vector2 movement, currentVel, desiredVel, steering;
     private float speed = 5.0f;
     private float angle;
     // Start is called before the first frame update
     void Start() {
         rigidBody = this.GetComponent<Rigidbody2D>();
         enemy = GameObject.FindWithTag("Enemy");
+        currentVel.x = 0.0f;
+        currentVel.y = 0.0f;
 
     }
 
@@ -19,10 +21,15 @@ public class Flee : MonoBehaviour {
     void Update() {
 
         //use vector subtraction to find vector between enemy and current object
-        Vector2 direction = -(enemy.transform.position - gameObject.transform.position);
+        Vector2 direction = gameObject.transform.position - enemy.transform.position;
+
         angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
         rigidBody.rotation = angle;
+
+        //Normalize direction vector to get the desired velcoty
         direction.Normalize();
-        rigidBody.MovePosition((Vector2)gameObject.transform.position + (direction * speed * Time.deltaTime));
+        steering = direction - currentVel;
+        currentVel = currentVel + steering;
+        rigidBody.MovePosition((Vector2)gameObject.transform.position + (currentVel * speed * Time.deltaTime));
     }
 }
--- GAIT-G12/Assets/Persue.cs
+++ GAIT-G12/Assets/Persue.cs
@@ -6,23 +6,29 @@ public class Persue : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
     private GameObject player;
-    private Vector2 movement;
+    private Vector2 movement, currentVel, desiredVel, steering;
     private float speed = 5.0f;
     private float angle;
     // Start is called before the first frame update
     void Start() {
         rigidBody = this.GetComponent<Rigidbody2D>();
         player = GameObject.FindWithTag("Player");
-
+        currentVel.x = 0.0f;
+        currentVel.y = 0.0f;
     }
 
     // Update is called once per frame
     void Update() {
         //use vector subtraction to find vector between player and current object
         Vector2 direction = player.transform.position - gameObject.transform.position;
+
         angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
         rigidBody.rotation = angle;
+
+        //Normalize direction vector to get the desired velcoty
         direction.Normalize();
-        rigidBody.MovePosition((Vector2)gameObject.transform.position + (direction * speed * Time.deltaTime));
+        steering = direction - currentVel;
+        currentVel = currentVel + steering;
+        rigidBody.MovePosition((Vector2)gameObject.transform.position + (currentVel * speed * Time.deltaTime));
     }
 }
