233ebf1603f148e2e533b2ebb83bd2349eb44856
ajmeraayam <ajmeraayam@gmail.com>
2020-08-12 15:42:55 +1000

    Basic Leader following with Rigidbodies

--- GAIT-G12/Assets/Scripts/Motion.cs
+++ GAIT-G12/Assets/Scripts/Motion.cs
@@ -6,18 +6,28 @@ public class Motion : MonoBehaviour
 {
     private bool collided = false;
     [Tooltip("Positive value to move forward\nNegative to move backward")][SerializeField] float horizontalspeed = 2f;
+    //Rigidbody rigidbody;
     
+    void Start()
+    {
+        //rigidbody = GetComponent<Rigidbody>();
+    }
     // Update is called once per frame
     void Update()
     {
-        if(!collided)
-        {
+        //if(!collided)
+        //{
             transform.Translate(horizontalspeed * Vector3.forward * Time.deltaTime);
-        }
+        //}
     }
 
     void OnCollisionEnter(Collision collision)
     {
         collided = true;
+    }
+
+    public float Horizontalspeed()
+    {
+        return horizontalspeed;
     }
 }
---
+++ GAIT-G12/Assets/Scripts/ProjectVars.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+/*
+Class that holds all the project wide variables
+*/
+public class ProjectVars
+{
+    public static float LEADER_BEHIND_DIST = 10f;
+    public static float MAX_SEPARATION = 8f;
+    public static float LEADER_SIGHT_RADIUS = 20f;
+    public static float SLOWING_RADIUS = 2f;
+}
