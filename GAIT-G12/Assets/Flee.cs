82e5adeb69eb9dc71a78e7b4d958cb0104f9eb2b
rmit-s3722631-Daniel-Snowsill <41409058+rmit-s3722631-Daniel-Snowsill@users.noreply.github.com>
2020-08-12 17:35:31 +1000

    Finished Persue and Flee. Added Mouse movement
    completed persue and flee behaviours, also added script that allows game object to follow mouse

--- GAIT-G12/Assets/Flee.cs
+++ GAIT-G12/Assets/Flee.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class Flee : MonoBehaviour {
     private Rigidbody2D rigidBody;
     private GameObject enemy;
-    private Vector2 movement, currentVel, desiredVel, steering;
-    private float speed = 5.0f;
-    private float angle;
+    private Vector2 movement, desiredVel, steering, enemyVel;
+    private float speed = 2.0f;
+    private float angle, interval;
+
+    public Vector2 currentVel;
+
+
     // Start is called before the first frame update
     void Start() {
         rigidBody = this.GetComponent<Rigidbody2D>();
         enemy = GameObject.FindWithTag("Enemy");
         currentVel.x = 0.0f;
         currentVel.y = 0.0f;
+        enemyVel = enemy.GetComponent<Persue>().currentVel;
 
     }
 
     // Update is called once per frame
     void Update() {
+        //Update the enemy's velocity
+        enemyVel = enemy.GetComponent<Persue>().currentVel;
+
+        //update interval used to find targets enemy's future position
+        interval = (gameObject.transform.position - enemy.transform.position).magnitude / speed;
 
         //use vector subtraction to find vector between enemy and current object
-        Vector2 direction = gameObject.transform.position - enemy.transform.position;
+        Vector2 direction = (Vector2)gameObject.transform.position - ((Vector2)enemy.transform.position + enemyVel * interval);
 
         angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
         rigidBody.rotation = angle;
@@ -31,5 +41,9 @@ public class Flee : MonoBehaviour {
         steering = direction - currentVel;
         currentVel = currentVel + steering;
         rigidBody.MovePosition((Vector2)gameObject.transform.position + (currentVel * speed * Time.deltaTime));
+
+
+        //rigidBody.MovePosition((Vector2)gameObject.transform.position + (currentVel * Time.deltaTime * speed));
+
     }
 }
--- GAIT-G12/Assets/MouseMove.cs
+++ GAIT-G12/Assets/MouseMove.cs
@@ -2,36 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Persue : MonoBehaviour
+public class MouseMove : MonoBehaviour
 {
+
     private Rigidbody2D rigidBody;
     private GameObject target;
-    private Vector2 desiredVel, steering, targetVel;
+    private Vector2 desiredVel, steering;
     private float speed = 5.0f;
-    private float angle, interval;
+    private float angle;
 
     public Vector2 currentVel;
-
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         rigidBody = this.GetComponent<Rigidbody2D>();
-        target = GameObject.FindWithTag("Player");
         currentVel.x = 0.0f;
         currentVel.y = 0.0f;
-        targetVel = target.GetComponent<Flee>().currentVel;
     }
 
     // Update is called once per frame
-    void Update() {
-        //Update the targets velocity
-        targetVel = target.GetComponent<Flee>().currentVel;
+    void Update()
+    {
 
-        //update interval used to find targets future position
-        interval = (target.transform.position - gameObject.transform.position).magnitude / speed;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         //use vector subtraction to find vector between player and current object
-        Vector2 direction = ((Vector2)target.transform.position + targetVel * interval) 
-            - (Vector2)gameObject.transform.position;
+        Vector2 direction = (Vector2)mousePos
+            -(Vector2)gameObject.transform.position;
 
         //find angle needed to rotate toward the target
         angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
@@ -42,8 +39,6 @@ public class Persue : MonoBehaviour
         steering = direction - currentVel;
         currentVel = currentVel + steering;
         rigidBody.MovePosition((Vector2)gameObject.transform.position + (currentVel * speed * Time.deltaTime));
-        //print("Current Vel " + targetVel);
-        //print("Position " + gameObject.transform.position);
-        print("Interval " + interval);
+
     }
 }
