c31ce0df4a005a3ba5693f282263da6584ab8f5b
Daniel <danielsnowsill@live.com.au>
2020-08-23 20:59:57 +1000

    Added player movement and projectile shooting

---
+++ GAIT-G12/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo) {
+        if(hitInfo.tag != "Player") {
+            Destroy(gameObject);
+        }
+    }
+
+}
--- GAIT-G12/Assets/Scripts/PlayerShoot.cs
+++ GAIT-G12/Assets/Scripts/PlayerShoot.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    public Transform shootPoint;
+    public GameObject bulletPrefab;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,12 @@ public class PlayerShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1")) {
+            shoot();
+        }
+    }
+
+    private void shoot() {
+        Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
     }
 }
