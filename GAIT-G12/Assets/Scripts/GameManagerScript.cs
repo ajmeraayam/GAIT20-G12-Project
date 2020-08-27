b34ad20461b0465e5e4d1e3b4b40eedd8fd403e1
rmit-s3722631-Daniel-Snowsill <41409058+rmit-s3722631-Daniel-Snowsill@users.noreply.github.com>
2020-08-27 19:05:06 +1000

    Added UI and game over screen

---
+++ GAIT-G12/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour {
+    private static GameManagerScript instance = null;
+    private GameObject gameOverUI;
+    public int score = 0;
+    public int playerHealth = 100;
+    public bool isMenu = true, gameOver = false;
+
+    public static GameManagerScript Instance {
+        get {
+            if (instance == null) {
+                GameObject GameManagerObject = new GameObject();
+                GameManagerObject.name = "GameManager";
+
+                instance = GameManagerObject.AddComponent<GameManagerScript>();
+            }
+            return instance;
+        }
+
+    }
+
+
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (playerHealth <= 0) {
+            GameOver();
+        }
+    }
+    public void GameOver() {
+
+        //Load the game over screen
+        gameOverUI = (GameObject)Resources.Load("Prefabs/GameOverCanvas");
+        Instantiate(gameOverUI);
+        score = 0;
+        playerHealth = 100;
+    }
+}
---
+++ GAIT-G12/Assets/Scripts/TryAgainScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TryAgainScript : MonoBehaviour
+{
+   public void playGame() {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+}
