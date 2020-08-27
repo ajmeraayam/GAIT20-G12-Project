74d20426eace61d2756b4b5f1c1cd199b364fffd
ajmeraayam <ajmeraayam@gmail.com>
2020-08-27 16:00:20 +1000

    Player health included in the scene

---
+++ GAIT-G12/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    float health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100f;
+    }
+
+    public void ReduceHealth()
+    {
+        health -= 5f;
+        print("Health - " + health);
+        if(health == 0f)
+        {
+            print("Gameobject destroyed");
+            Destroy(gameObject);
+        }
+    }
+}
