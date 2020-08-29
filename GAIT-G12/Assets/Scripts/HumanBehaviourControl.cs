7df8952d183d0e186025043a55ad92413f30620a
sherryysj <45263452+sherryysj@users.noreply.github.com>
2020-08-30 04:56:22 +1000

    human behaviour convert added

--- GAIT-G12/Assets/Scripts/HumanBehaviourControl.cs
+++ GAIT-G12/Assets/Scripts/HumanBehaviourControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,18 +7,78 @@ public class HumanBehaviourControl : MonoBehaviour
 {
     // behaviour script and player object
     public GameObject player;
+    public bool isFollowPlayer;
+    AILerp pathFollowCompo1;
+    AIDestinationSetter pathFollowCompo2;
+    Seeker pathFollowCompo3;
+    Flee flee;
+    Wander wander;
+
 
     // Start is called before the first frame update
     void Start()
     {
         // open wandering
+
+        pathFollowCompo1 = this.gameObject.GetComponent<AILerp>();
+        pathFollowCompo2 = this.gameObject.GetComponent<AIDestinationSetter>();
+        pathFollowCompo3 = this.gameObject.GetComponent<Seeker>();
+        flee = this.gameObject.GetComponent<Flee>();
+        wander = this.gameObject.GetComponent<Wander>();
+
+        wander.enabled = true;
+        pathFollowCompo1.enabled = false;
+        pathFollowCompo2.enabled = false;
+        pathFollowCompo3.enabled = false;
+        isFollowPlayer = false;
+        flee.enabled = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool shouldFollowPlayer = isInPlayerRange();
         // if in player range, turn off wandering/flee, turn on following
+        if (shouldFollowPlayer && isFollowPlayer == false)
+        {
+            wander.enabled = false;
+            flee.enabled = false;
+            pathFollowCompo1.enabled = true;
+            pathFollowCompo2.enabled = true;
+            pathFollowCompo3.enabled = true;
+            isFollowPlayer = true;
+            
+        }
+        else if (!shouldFollowPlayer && isFollowPlayer != false)
+        {
+            flee.enabled = false;
+            pathFollowCompo1.enabled = false;
+            pathFollowCompo2.enabled = false;
+            pathFollowCompo3.enabled = false;
+            isFollowPlayer = false;
+            wander.enabled = true;
+
+        }
         // if being chased by zombie and not following player, turn off wandering, turn on flee
 
+    }
+
+    private bool isInPlayerRange()
+    {
+
+        if (Vector3.Distance(transform.position, player.transform.position) < 30)
+            return true;
+        return false;
+    }
+
+    // call by zombie.pursue
+    public void startFlee()
+    {
+        if (!isFollowPlayer)
+        {
+            wander.enabled = false;
+            flee.enabled = true;
+        }
     }
 }
